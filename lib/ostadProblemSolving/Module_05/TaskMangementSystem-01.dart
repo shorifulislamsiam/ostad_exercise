fff30322530e514f1c5e30fa31b1186e68288dc0
siam <shorifulislamsiam18@gmail.com>
2025-01-02 22:42:47 +0600

    updated

--- lib/ostadProblemSolving/Module_05/TaskMangementSystem-01.dart
+++ lib/ostadProblemSolving/Module_05/TaskMangementSystem-01.dart
@@ -4,9 +4,9 @@ class Task {
   String? description;
   String? status;
   //List <String?> status =["Pending","Progress","Completed"];
-  late DateTime dueDate;
+  DateTime dueDate;
 
-  Task(this.id, this.title, this.status);
+  Task(this.id, this.title, this.status, this.dueDate);
 
   //void add(Task task) {}
 }
@@ -26,19 +26,23 @@ class TaskManager {
       }
     }
   }
+  List<Task> showDueDate(){
+    return tasks.where((task) => task.dueDate.isBefore(DateTime.now())).toList();
+  }
 
   void listtask() {
     for (var task in tasks) {
-      print('ID: ${task.id}, Title: ${task.title}, Status: ${task.status}');
+      print('ID: ${task.id}, Title: ${task.title}, Status: ${task.status}, DateTime:${task.dueDate}');
     }
   }
+
 
 
 }
 main() {
   var taskManager = TaskManager();
-  taskManager.addTask(Task(1, 'Task 1', 'Pending'));
-  taskManager.addTask(Task(2, 'Task 2', 'In Progress'));
+  taskManager.addTask(Task(1, 'Task 1', 'Pending', DateTime(2025,01,02)));
+  taskManager.addTask(Task(2, 'Task 2', 'In Progress',DateTime(2025,01,02)));
   print('All tasks:');
   taskManager.listtask();
   taskManager.updateTask(1, 'Completed');
